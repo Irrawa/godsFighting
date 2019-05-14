bca217f807d1ea46f6ad83f68772841b5a87f430
Irrawa <bill-deng@outlook.com>
2019-05-15 00:49:42 +0800

    Added field saving data structure and set functions
    Next thing to do is adding get functions of the saving data structure.

--- godFighting.h
+++ godFighting.h
@@ -27,7 +27,9 @@ private:
     int sta_dh, sta_dm, sta_da, sta_dd, sta_ds,
     //状态造成的生命值、魔法值、攻击、防御、速度 的变化
         sta_Odh, sta_Odm, sta_Oda, sta_Odd, sta_Ods,
+    //对对手造成的...变化
         iniT, nT, sta_id;
+    //初始持续时间 目前持续时间 状态id
     vector <status> sta_rmStat;
     bool sta_pos, sta_neg;
 
@@ -82,6 +84,82 @@ private:
 
 };
 
+class field_status{
+    //场地状态
+public:
+    void SetupStatus();
+    //状态被施加时，使用此函数初始化
+    void RefStatus();
+    //每经过一回合，所有生效的status需要使用此函数刷新。
+    void StatusTakeEffect(character maker, character non_maker);
+    //效果产生影响
+    void StatusEnd();
+    //状态消失时调用的函数，主要功能为将一些状态造成的改变复原（如攻击力降低等）
+
+private:
+    string sta_name, sta_info;
+    //      状态名    状态介绍
+    character affecter;
+    //        状态生效的角色
+    int sta_dh, sta_dm, sta_da, sta_dd, sta_ds,
+    //状态对造成的生命值、魔法值、攻击、防御、速度 的变化
+            iniT, nT, sta_id;
+    //初始持续时间 目前持续时间 状态id
+    vector <status> sta_rmStat;
+    bool sta_pos, sta_neg;
+};
+
+class note_page{
+private:
+    int numTurn;
+    vector <character> StartCharacterL(2);
+    vector <field_status> StartFStatusL(10);
+    vector <character> CSerie(10); // 依顺序行动的角色
+    vector <cmove> MSerie(10); //依顺序施放的技能
+    character checkedCharacter1;
+    character checkedCharacter2;
+
+public:
+    note_page(int npage){
+        numTurn = npage;
+    }
+
+    set_start_characterL(vector <character> SC){
+        StartCharacter[0] = SC[0];
+        StartCharacter[1] = SC[1];
+    }
+
+    set_start_field_statusL(vector <field_status> SFS){
+        int length = FSF.size();
+        for (int i = 0; i < length; i++){
+            StartFStatusL[i] = SFS[i];
+        }
+    }
+
+    add_action(character acter, cmove act){ //添加角色和其行动
+        CSerie.push_back(acter);
+        MSerie.push_back(act);
+    }
+
+    set_C1(character acter){   //快速角色状态清算后记录
+        checkedCharacter1 = acter;
+    }
+
+    set_C2(character acter){   //慢速角色状态清算后记录
+        checkedCharacter2 = acter;
+    }
+    //****上次写到这里！5-15***接下来改写get函数
+
+    //   每回合开始时（也是场地状态清算后的情况）：{character Irrawa, character Mew},{field_status, numTurn, }
+    //   技能施放阶段：{character 行动方, cmove 行动技能, character 行动方, cmove 行动技能...}（一方可能行动多次）
+    //   速度较快的一方状态清算后： {character 速度较快的角色}
+    //   速度较慢的一方状态清算后： {character 速度较慢的角色}
+
+    set_startTurn_characters
+
+
+};
+
 
 class field{
 public:
@@ -90,11 +168,14 @@ public:
 
 private:
     vector <field_status> FStatusL;
-
     // 场地状态列表
 
+    vector <note_page> BattleRecord;
     //   ！！！需要设计一个可以记录全部战斗流程的数据结构！！！
-
+    //   每回合开始时（也是场地状态清算后的情况）：{character Irrawa, character Mew},{field_status, numTurn, }
+    //   技能施放阶段：{character 行动方, cmove 行动技能, character 行动方, cmove 行动技能...}（一方可能行动多次）
+    //   速度较快的一方状态清算后： {character 速度较快的角色}
+    //   速度较慢的一方状态清算后： {character 速度较慢的角色}
 };
 
 #endif //GODS_GODFIGHTING_H
